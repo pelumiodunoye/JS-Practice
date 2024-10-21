4056733e9687ebbda4ede164ad912bcc7885ac14
Pelumi Odunoye <odunoyepelumi23@gmail.com>
2024-10-21 13:52:46 +0100

    Completed: color Flipper project done

--- Color Flipper/colorflipper.js
+++ Color Flipper/colorflipper.js
@@ -17,28 +17,19 @@ if (window.location.pathname.includes("colorflipper.html")) {
   document
     .querySelector(".actionBtn")
     .addEventListener("click", function (changeColor) {
-      //Generate a random index
       let randomIndex = Math.floor(Math.random() * colors.length);
-      //select the color from the array
       let randomColor = colors[randomIndex];
-      //change the background color
       document.body.style.backgroundColor = randomColor;
-      //select element for color display (span)
       let colorNameDisplay = document.querySelector(".color");
-      //change default name to actual color name
       colorNameDisplay.textContent = randomColor;
-      // Match color name with color
       colorNameDisplay.style.color = randomColor;
     });
 } else if (window.location.pathname.includes("colorflipperhex.html")) {
-  // Create a function that generates random hex colors
   function randomHexColor() {
     let hex = "#";
     const hexValues = "0123456789ABCDEF";
     for (let i = 0; i < 6; i++) {
-      // Generate a random index to pick a character from hexChars
       let randomIndex = Math.floor(Math.random() * hexValues.length);
-      // Add the selected character to the hex string
       hex += hexValues[randomIndex];
     }
 
@@ -52,14 +43,3 @@ if (window.location.pathname.includes("colorflipper.html")) {
     colorNameDisplay.style.color = randomColor;
   });
 }
-
-//   Key concepts covered:
-
-// arrays
-// document.getElementById()
-// document.querySelector()
-// addEventListener()
-// document.body.style.backgroundColor
-// Math.floor()
-// Math.random()
-// array.length
